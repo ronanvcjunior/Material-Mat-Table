e4475f552fd313f15a0719e1a92918349cc9e251
Ronan Vieira <ronanvcjunior@gmail.com>
2021-10-27 06:07:12 -0300

    produtoEstoque Pagiable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import {  Component, OnInit, ViewChild } from '@angular/core' 
+import {  AfterViewInit, Component, OnInit, ViewChild } from '@angular/core' 
 import { MatPaginator } from '@angular/material/paginator' 
 import { MatDialog } from '@angular/material/dialog' 
 import { MatTableDataSource } from '@angular/material/table' 
 import { MatSort, Sort } from '@angular/material/sort' 
-import { ProdutoEstoque } from '../app/model/produto.model'
+import { ProdutoEstoque } from './model/produtoEstoque.model'
 import { ProdutoEstoqueService } from '../app/service/ProdutoEstoque.service'
+import { merge, Observable, of as observableOf } from 'rxjs';
+import { catchError, map, startWith, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -12,7 +14,15 @@ import { ProdutoEstoqueService } from '../app/service/ProdutoEstoque.service'
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent implements OnInit{
+export class AppComponent implements AfterViewInit{
+
+  resultsLength = 0;
+  isLoadingResults = true;
+  isRateLimitReached = false;
+
+  filterValue!: string
+
+  produtoEstoques: ProdutoEstoque[] = []
 
   displayedColumns: string[] = ['idProdutoEstoque', 'grupo', 'nmProduto', 'prUnitario', 'qtdEstoque', 'qtdReservada'] 
   dataSource!: MatTableDataSource<ProdutoEstoque> 
@@ -38,14 +48,50 @@ export class AppComponent implements OnInit{
 
   }
 
-  ngOnInit() {
-    this.atualizarDados()  
+  atualizarDadosPage(): void {
+    this.sort.sortChange.subscribe(() => this.paginator.pageIndex = 0)
+
+    merge(this.sort.sortChange, this.paginator.page)
+        .pipe(
+              startWith({}),
+              switchMap(() => {
+                this.isLoadingResults = true;
+                return this.service.findAllPaginator(
+                      this.sort.active, 
+                      this.sort.direction, 
+                      this.paginator.pageIndex, 
+                      this.paginator.pageSize,
+                      this.filterValue
+                ).pipe(catchError(() => observableOf(null)))
+              }),
+              map(data => {
+                  this.isLoadingResults = false
+                  this.isRateLimitReached = data ===null
+                  console.log(data)
+                  if (data === null) {
+                    return []
+                  }
+
+                  this.resultsLength = data.totalElements
+                  return data.content
+                })
+        ).subscribe((produtos => {
+      this.dataSource = new MatTableDataSource(produtos)
+      this.dataSource.paginator = this.paginator
+      this.dataSource.sort = this.sort
+      console.log(produtos)
+    }))
+
+  }
+
+  ngAfterViewInit() {
+    this.atualizarDadosPage()
   }
 
   applyFilter(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value 
-    this.dataSource.filter = filterValue.trim().toLowerCase() 
-
+    this.filterValue = (event.target as HTMLInputElement).value.trim().toLowerCase()
+    // this.dataSource.filter = this.filterValue
+    this.atualizarDadosPage()
     if (this.dataSource.paginator) {
       this.dataSource.paginator.firstPage() 
     }
--- src/app/service/ProdutoEstoque.service.ts
+++ src/app/service/ProdutoEstoque.service.ts
@@ -1,8 +1,10 @@
 import { HttpClient } from '@angular/common/http'
 import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { SortDirection } from '@angular/material/sort';
 import { Observable } from 'rxjs';
-import { ProdutoEstoque } from '../model/produto.model'
+import { ProdutoEstoque } from '../model/produtoEstoque.model'
+import { produtoEstoquePage } from '../model/produtoEstoquePage.model';
 
 @Injectable({
     providedIn: 'root'
@@ -12,23 +14,47 @@ export class ProdutoEstoqueService {
     urlBase: string = "http://localhost:8080/produtoEstoque"
 
     constructor(
-      private http: HttpClient,
-      private snackBar: MatSnackBar
+        private http: HttpClient,
+        private snackBar: MatSnackBar
     ) { }
 
     showMessage(msg: string, isError: boolean = false): void {
         this.snackBar.open(msg, "Fechar",
-        {
-          verticalPosition:'top',
-          horizontalPosition: 'right',
-          duration: 3000,
-          panelClass: isError ? ['msg-error'] : ['msg-success']
-        }
+            {
+                verticalPosition: 'top',
+                horizontalPosition: 'right',
+                duration: 3000,
+                panelClass: isError ? ['msg-error'] : ['msg-success']
+            }
         )
-    
+
     }
 
-    findAll() : Observable<ProdutoEstoque[]> {
-        return this.http.get<ProdutoEstoque[]>(this.urlBase);
+    findAll(): Observable<ProdutoEstoque[]> {
+        return this.http.get<ProdutoEstoque[]>(this.urlBase)
+    }
+
+    findAllPaginator(sort: string, order: SortDirection, page: number, size: number, filter: string): Observable<produtoEstoquePage> {
+        console.log(sort);
+        console.log(order);
+        
+        
+        if (filter === undefined) {
+            filter = ''
+        }
+
+        let url: string
+        if (sort == 'idProdutoEstoque' || sort == 'qtdEstoque' || sort == 'qtdReservada') {
+            url = `${this.urlBase}/page/?nmProduto=${filter}&page=${page}&size=${size}&sort=${sort},${order}`
+        } else if(sort === undefined) {
+            url = `${this.urlBase}/page/?nmProduto=${filter}&page=${page}&size=${size}&sort=idProdutoEstoque`
+        } else if (order === '') {
+            url = `${this.urlBase}/page/?nmProduto=${filter}&page=${page}&size=${size}&sort=idProdutoEstoque`
+        } else {
+            url = `${this.urlBase}/page/?nmProduto=${filter}&page=${page}&size=${size}&sort=produto.${sort},${order}`
+        }
+
+        console.log(url)        
+        return this.http.get<produtoEstoquePage>(url)
     }
 }
